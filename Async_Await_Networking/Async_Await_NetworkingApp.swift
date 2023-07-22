1a6fa286a6e4cd889592babdc87b75dd4fd3a26c
Rashad Abdul-Salaam <upennmobile@gmail.com>
2023-07-21 22:18:07 -0400

    Initial Commit

---
+++ Async_Await_Networking/Async_Await_NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Async_Await_NetworkingApp.swift
+//  Async_Await_Networking
+//
+//  Created by Rashad Abdul-Salam on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Async_Await_NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Async_Await_Networking/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Async_Await_Networking
+//
+//  Created by Rashad Abdul-Salam on 7/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
