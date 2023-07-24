e8c84d948f65d200688c864e2e445a2d6a57a85e
Rashad Abdul-Salaam <upennmobile@gmail.com>
2023-07-24 13:21:17 -0400

    Refactor Mapping + add gitignore

--- Async_Await_Networking/Mapping/MapView.swift
+++ Async_Await_Networking/Mapping/MapView.swift
@@ -7,139 +7,7 @@
 
 import SwiftUI
 import MapKit
-import CoreLocation
 
-class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    
-    @Published var mapView = MKMapView()
-    // Region...
-    @Published var region: MKCoordinateRegion!
-    // Alert.....
-    @Published var permissionDenied = false
-    // Map Type
-    @Published var mapType: MKMapType = .standard
-    // Search Text
-    @Published var searchText = ""
-    // Searched Places
-    @Published var places = [Place]()
-    // Selected Place
-    @Published var selectedPlace: Place?
-    // Selected Annotation
-    @Published var selectedAnnotation: MKAnnotation?
-    // Directions Steps
-    @Published var directions = [String]()
-    
-    var userLocationCoordinates: CLLocationCoordinate2D? {
-        return mapView.annotations.filter { $0.isKind(of: MKUserLocation.self) }.first?.coordinate
-    }
-    
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        switch manager.authorizationStatus {
-        case .denied:
-            permissionDenied.toggle()
-        case .notDetermined:
-            manager.requestWhenInUseAuthorization()
-        case .authorizedWhenInUse:
-            manager.requestLocation()
-        default: ()
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("LocationManager Error: \(error.localizedDescription)")
-    }
-    
-    // Getting user Region.....
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else { return }
-        
-        self.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
-        
-        // Updating Map.....
-        self.mapView.setRegion(self.region, animated: true)
-        
-        // Smooth Animations....
-        self.mapView.setVisibleMapRect(self.mapView.visibleMapRect, animated: true)
-    }
-    
-    func searchQuery() {
-        let request = MKLocalSearch.Request()
-        request.naturalLanguageQuery = searchText
-        
-        selectedPlace = nil
-        places.removeAll()
-        
-        // Fetch....
-        MKLocalSearch(request: request).start { (response, _) in
-            guard let result = response else { return }
-            
-            self.places = result.mapItems.compactMap({ item -> Place? in
-                return Place(place: item.placemark)
-            })
-        }
-    }
-    
-    // Pick Search Result
-    
-    func selectPlace(place: Place) {
-        
-        // Show Pin on Map
-        
-        searchText = ""
-        selectedPlace = place
-        
-        guard let coordinate = place.place.location?.coordinate else { return }
-        
-        let pointAnnotation = MKPointAnnotation()
-        pointAnnotation.coordinate = coordinate
-        pointAnnotation.title = place.place.name ?? "No Name"
-        
-        // Remove All Old Annotations
-        mapView.removeAnnotations(mapView.annotations)
-        mapView.addAnnotation(pointAnnotation)
-        
-        // Move Map to that Location....
-        
-        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
-        
-        mapView.setRegion(coordinateRegion, animated: true)
-        mapView.setVisibleMapRect(mapView.visibleMapRect, animated: true)
-    }
-    
-    func makeDirections() {
-        guard
-            let selectionCoordinates = selectedPlace?.place.location?.coordinate,
-            let userCoordinates = userLocationCoordinates
-        else { return }
-        
-        let p1 = MKPlacemark(coordinate: userCoordinates)
-        let p2 = MKPlacemark(coordinate: selectionCoordinates)
-        
-        let request = MKDirections.Request()
-        request.source = MKMapItem(placemark: p1)
-        request.destination = MKMapItem(placemark: p2)
-        request.transportType = .automobile
-        
-        let directions = MKDirections(request: request)
-        directions.calculate { response, error in
-            guard let route = response?.routes.first else { return }
-//            mapView.addAnnotation([p1, p2]) Not needed?
-            self.mapView.addOverlay(route.polyline)
-            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20), animated: true)
-            
-            // Set Direction Steps
-            self.directions = route.steps.map { $0.instructions }.filter { !$0.isEmpty }
-        }
-    }
-}
-
-extension MapViewModel: AnnotationSelectionDelegate {
-    
-    func selectedAnnotation(_ annotation: MKAnnotation) {
-        selectedAnnotation = annotation
-    }
-}
 
 struct Place: Identifiable {
     var id = UUID().uuidString
@@ -149,7 +17,6 @@ struct Place: Identifiable {
 
 struct MapView: View {
     
-//    @State private var directions = [String]()
     @State private var showDirections = false
     @StateObject var mapData = MapViewModel()
     @State var locationManager = CLLocationManager()
@@ -157,35 +24,17 @@ struct MapView: View {
     var body: some View {
         VStack {
             ZStack {
-                
                 MapViewContent(directions: $mapData.directions)
                     .environmentObject(mapData)
                     .ignoresSafeArea(.all, edges: .all)
                 
                 VStack {
                     searchBarView
-                    
                     Spacer()
                 }
-                
-                
             }
             .sheet(isPresented: $showDirections) {
-                VStack {
-                    Text("Directions")
-                        .font(.largeTitle)
-                        .bold()
-                        .padding()
-                    
-                    Divider().background(.blue)
-                    
-                    List {
-                        ForEach(0..<mapData.directions.count, id: \.self) { i in
-                            Text(mapData.directions[i])
-                                .padding()
-                        }
-                    }
-                }
+                directionsSheetUI
             }
             .onAppear(perform: {
                 locationManager.delegate = mapData
@@ -225,15 +74,22 @@ struct MapView: View {
     
     var searchBarView: some View {
         VStack(spacing: 0) {
-            HStack {
-                Image(systemName: "magnifyingglass")
-                    .foregroundColor(.gray)
-                TextField("Search", text: $mapData.searchText)
+            ZStack {
+                // Search Bar
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(.gray)
+                    TextField("Search", text: $mapData.searchText)
+                }
+                .padding(.vertical, 10)
+                .padding(.horizontal)
+                .background(.white)
+                
             }
-            .padding(.vertical, 10)
-            .padding(.horizontal)
-            .background(.white)
             
+            Divider()
+            
+            // Search Results
             if !mapData.places.isEmpty && !mapData.searchText.isEmpty {
                 searchResultsView
             }
@@ -260,6 +116,34 @@ struct MapView: View {
         }
         .background(.white)
     }
+    
+    var directionsSheetUI: some View {
+        VStack {
+            Text("Directions")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+            
+            List {
+                ForEach(0..<mapData.directions.count, id: \.self) { i in
+                    Text(mapData.directions[i])
+                        .padding()
+                }
+            }
+        }
+    }
+    
+    var mappingDirectionsButton: some View {
+        
+        // Mapping Directions button
+        Button {
+//            mapData.makeDirections()
+        } label: {
+            Image(systemName: "map")
+        }
+//        .disabled(mapData.selectedAnnotation == nil)
+        .padding(.trailing)
+    }
 }
 
 struct MapViewContent: UIViewRepresentable {
@@ -276,16 +160,11 @@ struct MapViewContent: UIViewRepresentable {
         view.showsUserLocation = true
         view.delegate = context.coordinator
         
-//        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: <#T##CLLocationDegrees#>, longitude: <#T##CLLocationDegrees#>), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-//        view.setRegion(mapData.region, animated: true)
         return view
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-//        let mapView = MKMapView()
-//        mapView.delelgate = context.coordinator
-        let view = mapData.mapView
-        view.delegate = context.coordinator
+
     }
     
     class MapViewCoordinator: NSObject, MKMapViewDelegate {
@@ -312,7 +191,7 @@ struct MapViewContent: UIViewRepresentable {
         }
         
         func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-            let renderer = MKMultiPolylineRenderer(overlay: overlay)
+            let renderer = MKPolylineRenderer(overlay: overlay)
             renderer.strokeColor = .blue
             renderer.lineWidth = 5
             return renderer
@@ -326,7 +205,6 @@ struct MapViewContent: UIViewRepresentable {
             annotationDelegate?.selectedAnnotation(annotation)
         }
     }
-    
 }
 
 protocol AnnotationSelectionDelegate: AnyObject {
---
+++ Async_Await_Networking/Mapping/MapViewModel.swift
@@ -0,0 +1,159 @@
+//
+//  MapViewModel.swift
+//  Async_Await_Networking
+//
+//  Created by Rashad Abdul-Salam on 7/23/23.
+//
+
+import Foundation
+import MapKit
+import CoreLocation
+
+class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    @Published var mapView = MKMapView()
+    // Region...
+    @Published var region: MKCoordinateRegion?
+    // Alert.....
+    @Published var permissionDenied = false
+    // Map Type
+    @Published var mapType: MKMapType = .standard
+    // Search Text
+    @Published var searchText = ""
+    // Searched Places
+    @Published var places = [Place]()
+    // Selected Place
+    @Published var selectedPlace: Place?
+    // Directions Steps
+    @Published var directions = [String]()
+    
+    var userLocationCoordinates: CLLocationCoordinate2D? {
+        return mapView.annotations.filter { $0.isKind(of: MKUserLocation.self) }.first?.coordinate
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .denied:
+            permissionDenied.toggle()
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .authorizedWhenInUse:
+            manager.requestLocation()
+        default: ()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("LocationManager Error: \(error.localizedDescription)")
+    }
+    
+    // Getting user Region.....
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        self.region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        
+        self.setRegion()
+    }
+    
+    func setRegion(_ region: MKCoordinateRegion? = nil) {
+        guard let region = region else {
+            self.region = MKCoordinateRegion(center: userLocationCoordinates!, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            setRegion(self.region)
+            return
+        }
+        // Updating Map.....
+        self.mapView.setRegion(region, animated: true)
+        
+        // Smooth Animations....
+        self.mapView.setVisibleMapRect(self.mapView.visibleMapRect, animated: true)
+    }
+    
+    func searchQuery() {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchText
+        
+        places.removeAll()
+        
+        // Fetch....
+        MKLocalSearch(request: request).start { (response, _) in
+            guard let result = response else { return }
+            
+            self.places = result.mapItems.compactMap({ item -> Place? in
+                return Place(place: item.placemark)
+            })
+        }
+    }
+    
+    // Pick Search Result
+    
+    func selectPlace(place: Place) {
+        
+        // Refresh Content
+        refreshContent()
+        
+        // Show Pin on Map
+        selectedPlace = place
+        
+        guard let coordinate = place.place.location?.coordinate else { return }
+        
+        let pointAnnotation = MKPointAnnotation()
+        pointAnnotation.coordinate = coordinate
+        pointAnnotation.title = place.place.name ?? "No Name"
+        
+        // Remove All Old Annotations
+        
+        mapView.addAnnotation(pointAnnotation)
+        
+        // Move Map to that Location....
+        
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        
+        setRegion(coordinateRegion)
+    }
+    
+    func makeDirections(coordinate: CLLocationCoordinate2D) {
+        guard let userCoordinates = userLocationCoordinates
+        else { return }
+        
+        let p1 = MKPlacemark(coordinate: userCoordinates)
+        let p2 = MKPlacemark(coordinate: coordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: p1)
+        request.destination = MKMapItem(placemark: p2)
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        directions.calculate { response, error in
+            guard let route = response?.routes.first else { return }
+
+            self.mapView.addOverlay(route.polyline)
+            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20), animated: true)
+            
+            // Set Direction Steps
+            self.directions = route.steps.map { $0.instructions }.filter { !$0.isEmpty }
+        }
+    }
+    
+    func makeDirections(with annotation: MKAnnotation) {
+        let coordinate = annotation.coordinate
+        self.makeDirections(coordinate: coordinate)
+    }
+    
+    private func refreshContent() {
+        selectedPlace = nil
+        searchText = ""
+        directions = []
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.removeOverlays(mapView.overlays)
+    }
+}
+
+extension MapViewModel: AnnotationSelectionDelegate {
+    
+    func selectedAnnotation(_ annotation: MKAnnotation) {
+        self.makeDirections(with: annotation)
+    }
+}
