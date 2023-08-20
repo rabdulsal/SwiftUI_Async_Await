9841b950ea9b58bbe823189788985ee7a23b2266
Rashad Abdul-Salaam <upennmobile@gmail.com>
2023-08-20 13:23:29 -0400

    Add 'In Transit' label

--- Async_Await_Networking/CustomProgressBar.swift
+++ Async_Await_Networking/CustomProgressBar.swift
@@ -14,6 +14,7 @@ struct ProgressBarView: View {
     var body: some View {
         VStack {
             CustomProgressBar(value: $progressValue).frame(height: 20)
+                .padding(.bottom, 30)
             
             Button {
                 self.startProgressBar()
@@ -32,9 +33,10 @@ struct ProgressBarView: View {
     }
     
     func startProgressBar() {
-        for _ in 0...80 {
-            self.progressValue += 0.015
-        }
+        //        for _ in 0...80 {
+        //            self.progressValue += 0.015
+        //        }
+        self.progressValue += 0.15
     }
     
     func resetProgressBar() {
@@ -42,21 +44,43 @@ struct ProgressBarView: View {
     }
 }
 
+/**
+ Abstract:
+ Add'l features:
+ 1. "In Transit" and "Completed" text trailing-aligned to progress bar
+ 2. Change color from blue to gray
+ 3. Sectioning of each portion of the progress bar + label for proportional count of each section
+ */
 struct CustomProgressBar: View {
     
     @Binding var value: Float
+    var totalProgress: Float = 1.0
     
     var body: some View {
         GeometryReader { geometry in
-            ZStack(alignment: .leading) {
-                Rectangle().frame(width: geometry.size.width, height: geometry.size.height)
-                    .opacity(0.3)
-                    .foregroundColor(Color(UIColor.systemTeal))
+            VStack(alignment: .leading) {
                 
-                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.systemBlue))
-                    .animation(.linear)
-            }.cornerRadius(45.0)
+                Text("5 of 25")
+                ZStack(alignment: .leading) {
+                    Rectangle().frame(width: geometry.size.width, height: geometry.size.height)
+                        .opacity(0.3)
+                        .foregroundColor(Color(UIColor.systemTeal))
+                    
+                    ZStack(alignment: .trailing) {
+                            Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                                .foregroundColor(Color(UIColor.systemBlue))
+                                .animation(.linear)
+                        if self.value > 0.0 {
+                                
+                            Text("In Transit")
+                                .foregroundColor(.white)
+                                .fontWeight(.semibold)
+                                .padding(.trailing, 5)
+                                
+                        }
+                    }
+                }.cornerRadius(45.0)
+            }
         }
     }
 }
