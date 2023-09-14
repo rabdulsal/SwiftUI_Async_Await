ec1a957e20aec1d9416e1f9f1cd5b06cd8e4ff2b
Rashad Abdul-Salaam <rashad_the_god@Rashads-MacBook-Air.local>
2023-09-14 15:44:03 -0400

    Basic build-out of RouteDetailsView & navigation from Login -> RoutesList -> RouteDetails

--- Async_Await_Networking/Routes/RouteDetailsView.swift
+++ Async_Await_Networking/Routes/RouteDetailsView.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct RouteDetailsView: View {
+    let loadItem: RouteItem
+    
+    var transitStatus: String {
+        ""
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+        VStack {
+            // Transit Bar
+            HStack(alignment: .top, spacing: 10) {
+                VStack(alignment: .leading) {
+                    Text("In Transit")
+                    Text("Current Stop: \(loadItem.currentStopCount) of \(loadItem.totalStopCount)")
+                    
+                }
+                
+                VStack(alignment: .leading) {
+                    
+                    Text("Driver: \(loadItem.driver)")
+                    Text("Delivery @ \(loadItem.destination.title)")
+                }
+            }
+            
+            // Stops List
+            
+            
+            Spacer()
+        }
+        .navigationTitle(loadItem.id)
+        .navigationBarTitleDisplayMode(.inline)
+        .padding()
     }
 }
 
 struct RouteDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        RouteDetailsView()
+        RouteDetailsView(
+            loadItem: RouteItem(
+            id: "DEF",
+            stops: [
+                    RouteStop(id: "4", title: "", locationCode: "WMT-5280", stopNumber: 1, scheduledArrival: "", arrivalState: "previous"),
+                    RouteStop(id: "5", title: "", locationCode: "TNB-600159706", stopNumber: 2, scheduledArrival: "", arrivalState: "previous"),
+                    RouteStop(id: "6", title: "", locationCode: "TNB-600159706", stopNumber: 3, scheduledArrival: "", arrivalState: "current"),
+                    RouteStop(id: "7", title: "SBUX-ATL47931", locationCode: "WMT-2339", stopNumber: 4, scheduledArrival: "", arrivalState: "future")
+            ],
+            driver: "Prof. Dumbledore",
+            transitStatus: "in_transit")
+        )
     }
 }
--- Async_Await_Networking/Routes/RoutesListView.swift
+++ Async_Await_Networking/Routes/RoutesListView.swift
@@ -12,36 +12,44 @@ struct RoutesListView: View {
     @ObservedObject var viewModel = RoutesListViewModel()
     
     var body: some View {
-        ScrollView {
-            LazyVStack(alignment: .leading) {
-                ForEach(RoutesListViewModel.mockRoutes, id: \.self) { route in
-                    
-                    // TODO: Wrap in RoundedRect ZStack?
-                    VStack(alignment: .leading) {
-                        Text(route.id)
-                            .foregroundColor(.primary)
-                            .font(.system(size: 12, weight: .bold))
+        NavigationView {
+            ScrollView {
+                LazyVStack(alignment: .leading) {
+                    ForEach(RoutesListViewModel.mockRoutes, id: \.self) { route in
                         
-                        Text(route.transitStatusMessage)
-                            .foregroundColor(.secondary)
-                            .font(.system(size: 12))
-                        //                        .font(.caption)
-                        
-                        //                        }
-                        // Show Route ProgressBar
-                        CustomProgressBar(value: route.currentStopCount, maxProgress: route.totalStopCount, transitStatusType: route.transitStatusType).frame(height: CustomProgressBar.DefaultHeight)
+                        NavigationLink (destination: {
+                            RouteDetailsView(loadItem: route)
+                        }, label: {
+                            // TODO: Wrap in RoundedRect ZStack?
+                            
+                                VStack(alignment: .leading) {
+                                    Text(route.id)
+                                        .foregroundColor(.primary)
+                                        .font(.system(size: 12, weight: .bold))
+                                    
+                                    Text(route.transitStatusMessage)
+                                        .foregroundColor(.secondary)
+                                        .font(.system(size: 12))
+                                    //                        .font(.caption)
+                                    
+                                    //                        }
+                                    // Show Route ProgressBar
+                                    CustomProgressBar(value: route.currentStopCount, maxProgress: route.totalStopCount, transitStatusType: route.transitStatusType).frame(height: CustomProgressBar.DefaultHeight)
+                                }
+                                .padding()
+                                .border(.gray, width: 2.0)
+                                .cornerRadius(4.0)
+                                .background(.white)
+                        })
                     }
-                    .padding()
-                    .border(.gray, width: 2.0)
-                    .cornerRadius(4.0)
-                    .background(.white)
+                    .padding(EdgeInsets(top: 5, leading: 16, bottom: 5, trailing: 16))
                 }
-                .padding(EdgeInsets(top: 5, leading: 16, bottom: 5, trailing: 16))
+                
+                
             }
-            
-            
+            .navigationTitle("Routes In Transit")
+            .background(Color(uiColor: UIColor(red: 250.0, green: 250.0, blue: 250.0, alpha: 1)))
         }
-        .background(Color(uiColor: UIColor(red: 250.0, green: 250.0, blue: 250.0, alpha: 1)))
     }
 }
 
